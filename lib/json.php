758de88eeb67aaf2e368b84c1094a0b6a6f369a3
Patrick Gingras <775.pg.12@gmail.com>
2019-04-18 16:30:36 -0400

    added json parse_file function

--- lib/json.php
+++ lib/json.php
@@ -1,10 +1,18 @@
 <?php
 namespace lib\json;
 
-function parse(string $json) {
-    $value = \json_decode($json);
+function parse(...$args) {
+    $value = \json_decode(...$args);
     if ($value === null && \json_last_error() !== JSON_ERROR_NONE) {
-        throw new Error(\json_last_error_msg());
+        throw new \Error(\json_last_error_msg());
     }
     return $value;
+}
+
+function parse_file(string $filename, ...$options) {
+    if (!\file_exists($filename)) {
+        throw new \Error("File $filename does not exist");
+    }
+    $contents = \file_get_contents($filename);
+    return parse($contents, ...$options);
 }
