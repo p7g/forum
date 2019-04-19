cfc0280fed46a38c29607416cd55aa2c8f5e66a2
Patrick Gingras <775.pg.12@gmail.com>
2019-04-19 12:45:53 -0400

    test some db abstractions

--- public/index.php
+++ public/index.php
@@ -4,15 +4,40 @@ require \dirname($_SERVER['DOCUMENT_ROOT']) . '/index.php';
 use lib\db;
 use lib\http;
 use lib\http\{Request, Response};
+use lib\iter;
 use lib\view;
 
 configure();
 
+$create_hullo = db\prepare(<<<SQL
+    CREATE TABLE IF NOT EXISTS hullo (
+        id INTEGER PRIMARY KEY,
+        text TEXT
+    );
+SQL
+);
+db\execute($create_hullo);
+
+$seed_hullo = db\prepare(<<<SQL
+    INSERT INTO hullo (text)
+    VALUES
+        ('test'),
+        ('hullo');
+SQL
+);
+$query_hullo = db\prepare("SELECT * FROM hullo");
+
 http\handle(new class implements http\GETHandler, http\POSTHandler {
     public function get(Request $request, Response $response): void {
+        global $seed_hullo, $query_hullo;
+        $result = db\transact(function () use ($seed_hullo, $query_hullo) {
+            db\execute($seed_hullo);
+            return iter\collect(db\query_all($query_hullo));
+        });
+
         view\render('../view/home', (object) [
             'thing' => $request->get_query('thing'),
-            'request' => db\configuration()->get('dsn'),
+            'request' => $result,
         ]);
     }
 
