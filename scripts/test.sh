222b908a71a2d89732cd1d36ba39763b76744b7e
Patrick Gingras <775.pg.12@gmail.com>
2019-04-19 18:15:21 -0400

    added missing quotes

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ source "$(dirname "$0")/utils.sh"
 OLD_PWD="$PWD"
 
 cd "$(parentdir "$0")"
-php "$PWD/lib/test.php" --continue-on-fail $@ "$PWD/tests"
+php "$PWD/lib/test.php" --continue-on-fail "$@" "$PWD/tests"
 cd "$OLD_PWD"
